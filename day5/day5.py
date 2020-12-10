8569c071b9ea40979fe1d9c63bc13e8dbe74ffda
Håvard <8015540+HGodal@users.noreply.github.com>
2020-12-10 15:24:40 +0100

    Day 5

---
+++ day5/day5.py
@@ -0,0 +1,32 @@
+from operator import add
+import numpy as np
+
+directionDict = {
+    'V': [-1, 0],
+    'H': [1, 0],
+    'O': [0, 1],
+    'N': [0, -1]
+}
+
+
+def coordinate(direction):
+    return directionDict[direction]
+
+
+currentCoordinate = [0, 0]
+with open("day5.txt") as f:
+    for line in f:
+        xCoor = np.zeros(len(line))
+        yCoor = np.zeros(len(line))
+        for i in range(len(line)):
+            currentCoordinate = list(
+                map(add, currentCoordinate, coordinate(line[i])))
+            xCoor[i] = currentCoordinate[0]
+            yCoor[i] = currentCoordinate[1]
+
+
+def PolyArea(x, y):  # Shoelace area formula
+    return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
+
+
+print(PolyArea(xCoor, yCoor))
