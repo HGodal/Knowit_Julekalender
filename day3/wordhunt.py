5641651f8ac1b2078a4d1d9d84d13f4152c76d9c
Håvard <8015540+HGodal@users.noreply.github.com>
2020-12-10 15:24:33 +0100

    Day 3

--- day3/wordhunt.py
+++ day3/wordhunt.py
@@ -16,7 +16,7 @@ df = np.array(pd.read_csv('matrix.txt', sep=r'\s*', engine='python',
 for i in range(2):
     addLines(np.sum(df, axis=i))
     for j in range(-999, 1000):
-        addLines(str(np.diag(df, k=j).sum()).split(" "))
+        addLines([np.diag(df, k=j).sum()])
     df = np.fliplr(df)
 
 allWords = pd.read_csv('wordlist.txt', sep='\n', header=None)[0].tolist()
