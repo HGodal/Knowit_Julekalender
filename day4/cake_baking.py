4562ed06fd0873675bd391bc392784b509917bba
Håvard <8015540+HGodal@users.noreply.github.com>
2020-12-12 18:10:09 +0100

    Day 4

--- day4/cake_baking.py
+++ day4/cake_baking.py
@@ -1,13 +1,13 @@
-resources = {}
+from collections import defaultdict
+
+resources = defaultdict(int)
 
 with open("leveringsliste.txt") as f:
     for line in f:
         sanitized_line = line.replace(" ", "").split(",")
         for pair in sanitized_line:
             (key, val) = pair.split(":")
-            resources[str(key)] = resources[str(key)] + \
-                int(val) if key in resources.keys() else int(val)
-
+            resources[key] += int(val)
 
 ingredients = {"sukker": 2, "mel": 3, "melk": 3, "egg": 1}
 potential_cakes = []
